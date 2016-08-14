d8f2960a02bacd0e72951449817a7028cd6bce22
Joshua Comeau <joshwcomeau@gmail.com>
2016-08-14 15:42:50 -0400

    Create index of exports for Replay

--- replay/src/index.js
+++ replay/src/index.js
@@ -1,9 +1,29 @@
-import React from 'react'
+// Components
+import Backdrop from './components/Backdrop';
+import Cassette from './components/Cassette';
+import CassetteList from './components/CassetteList';
+import Icon from './components/Icon';
+import Replay from './components/Replay';
+import VCR from './components/VCR';
+import VCRButton from './components/VCRButton';
+import VCRPowerLight from './components/VCRPowerLight';
 
-export default React.createClass({
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
-})
+// Core logic
+import playActions from './playActions';
+import replayMiddleware from './replayMiddleware';
+import wrapReducer from './wrapReducer';
+
+
+export {
+  Backdrop,
+  Cassette,
+  CassetteList,
+  Icon,
+  Replay,
+  VCR,
+  VCRButton,
+  VCRPowerLight,
+  playActions,
+  replayMiddleware,
+  wrapReducer,
+};
