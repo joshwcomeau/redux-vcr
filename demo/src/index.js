e7f1695e8550a11e7b69f2f3bab5f96218c6f5cd
Joshua Comeau <joshwcomeau@gmail.com>
2016-08-14 13:49:01 -0400

    Annotate demo src

--- demo/src/index.js
+++ demo/src/index.js
@@ -7,23 +7,45 @@ import captureMiddleware from '../../capture/lib';
 import PersistHandler from '../../persist/lib';
 import RetrieveHandler from '../../retrieve/lib/retrieve-data-handler';
 import retrieveMiddleware from '../../retrieve/lib';
+import replayMiddleware from '../../replay/replay-middleware';
 
 import App from './components/App';
 import reducer from './reducers';
 import './index.css';
 
+// Firebase credentials are safe to distribute in the client;
+// on their own, they don't grant any authorization.
+// It's for this reason that Firebase was chosen, so that no server-side
+// authentication is required :)
 const firebaseAuth = {
   apiKey: 'AIzaSyDPq76JUdNtZcnileNl0fRpVtwGD4zgpjY',
   authDomain: 'redux-vcr-demo.firebaseapp.com',
   databaseURL: 'https://redux-vcr-demo.firebaseio.com',
 };
 
+// The PersistHandler handles submitting captured actions to Firebase.
+// The only required config is the firebaseAuth object.
+// This should be distributed to your users in production.
 const persister = new PersistHandler({ firebaseAuth });
+
+// Inversely, the RetrieveHandler pulls actions from Firebase, allowing
+// them to be replayed. It should only be included in development.
 const retriever = new RetrieveHandler({ firebaseAuth });
 
 const middlewares = [
+  // The capture middleware chronicles, filters, and timestamps actions
+  // as they're dispatched to the store. It needs to be passed the
+  // PersistHandler so it can send them to Firebase.
   captureMiddleware({ dataHandler: persister }),
+
+  // The retrieve middleware listens for specific actions dispatched
+  // from the Replay components, to fetch the recordings needed.
   retrieveMiddleware({ dataHandler: retriever }),
+
+  // Finally, the replay middleware is in charge of intercepting the
+  // PLAY_CASSETTE action, which allows previously-recorded sessions
+  // to be replayed.
+  replayMiddleware,
 ];
 
 const store = createStore(
