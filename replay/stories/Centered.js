f1187badd60d3db0d090636041479134e3eab786
Joshua Comeau <joshwcomeau@gmail.com>
2016-08-20 07:39:20 -0400

    Add helper component to center and pad Story components

---
+++ replay/stories/Centered.js
@@ -0,0 +1,31 @@
+import React, { PropTypes } from 'react';
+import classNames from 'classnames';
+
+const Centered = ({ children, vertical, horizontal, className, styles }) => {
+  const classes = classNames(className, {
+    'story-horizontally-centered': horizontal,
+    'story-vertically-centered': vertical,
+  });
+
+  return (
+    <div className={classes} style={styles}>
+      {children}
+    </div>
+  );
+};
+
+Centered.propTypes = {
+  children: PropTypes.node,
+  vertical: PropTypes.bool,
+  horizontal: PropTypes.bool,
+  className: PropTypes.string,
+  styles: PropTypes.object,
+};
+
+Centered.defaultProps = {
+  styles: { padding: '2rem' },
+  vertical: true,
+  horizontal: true,
+};
+
+export default Centered;
