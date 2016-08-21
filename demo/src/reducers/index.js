86f7c453b521b2426c5a96750814a51f940fbb4d
Joshua Comeau <joshwcomeau@gmail.com>
2016-08-21 09:58:35 -0400

    Update demo to not need to modify reducers

--- demo/src/reducers/index.js
+++ demo/src/reducers/index.js
@@ -1,8 +1,5 @@
 import { combineReducers } from 'redux';
-// import { reduxVCRReducer } from 'redux-vcr.replay';
-import { reduxVCRReducer } from '../../../replay/src';
 
 import answers from './answers.reducer';
 
-
-export default combineReducers({ answers, reduxVCR: reduxVCRReducer });
+export default combineReducers({ answers });
