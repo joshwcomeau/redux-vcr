649b7e6ad47cf361de33bb236d09ba81351096e0
Joshua Comeau <joshwcomeau@gmail.com>
2016-08-28 20:22:05 -0400

    Add root package. Rename 'shared' package

---
+++ packages/_root/index.js
@@ -0,0 +1,6 @@
+import * as capture from 'redux-vcr.capture';
+import * as persist from 'redux-vcr.persist';
+import * as retrieve from 'redux-vcr.retrieve';
+import * as replay from 'redux-vcr.replay';
+
+export { capture, persist, retrieve, replay };
