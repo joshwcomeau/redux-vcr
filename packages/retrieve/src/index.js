a630ad9df4abdbddc6f8bfcd33b4fa65542dab69
Joshua Comeau <joshwcomeau@gmail.com>
2016-10-01 07:14:57 -0400

    Slight refactor of module exports

--- packages/retrieve/src/index.js
+++ packages/retrieve/src/index.js
@@ -1,6 +1,5 @@
-import { getQueryParam } from 'redux-vcr.shared';
-
 import createRetrieveHandler from './create-retrieve-handler';
 import createRetrieveMiddleware from './create-retrieve-middleware';
 
-export { getQueryParam, createRetrieveHandler, createRetrieveMiddleware };
+export { getQueryParam } from 'redux-vcr.shared';
+export { createRetrieveHandler, createRetrieveMiddleware };
