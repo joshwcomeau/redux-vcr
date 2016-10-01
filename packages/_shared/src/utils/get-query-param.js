d465b61cce72a6253c22a2c4296e0d887f341210
Joshua Comeau <joshwcomeau@gmail.com>
2016-10-01 07:10:27 -0400

    Add util to get query param

---
+++ packages/_shared/src/utils/get-query-param.js
@@ -0,0 +1,10 @@
+export default function getQueryParam(name, url = window.location.href) {
+  // eslint-disable-next-line no-param-reassign
+  name = name.replace(/[\[\]]/g, '\\$&');
+  const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+  const results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
